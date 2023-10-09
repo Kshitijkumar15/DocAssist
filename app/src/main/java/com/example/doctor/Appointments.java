88dd120def5e3bf45e031b26202ada09cd556302
Kshitij kumar singh <kshitijkumarsingh58@gmail.com>
2023-10-09 19:28:57 +0530

    Work in progress

--- app/src/main/java/com/example/doctor/Appointments.java
+++ app/src/main/java/com/example/doctor/Appointments.java
@@ -2,27 +2,48 @@ package com.example.doctor;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.CalendarContract;
+import android.content.ContentResolver;
+import android.content.ContentValues;
+import android.content.Context;
+import android.widget.Toast;
+import java.util.TimeZone;
+
 
 
 import androidx.appcompat.app.AppCompatActivity;
 
 
-import java.util.Calendar;
 
 public class Appointments extends AppCompatActivity {
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_appointment);
+                // Create an intent to add a new event to the calendar
+                Intent intent = new Intent(Intent.ACTION_INSERT)
+                        .setData(CalendarContract.Events.CONTENT_URI)
+                        .putExtra(CalendarContract.Events.TITLE, "A Test Event from android app")
+                        .putExtra(CalendarContract.Events.ALL_DAY, true)
+                        .putExtra(CalendarContract.Events.EVENT_TIMEZONE, TimeZone.getDefault().getID())
+                        .putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME, System.currentTimeMillis())
+                        .putExtra(CalendarContract.EXTRA_EVENT_END_TIME, System.currentTimeMillis() + 60 * 60 * 1000);
 
-        Calendar cal = Calendar.getInstance();
-        Intent intent = new Intent(Intent.ACTION_EDIT);
-        intent.setType("vnd.android.cursor.item/event");
-        intent.putExtra("beginTime", cal.getTimeInMillis());
-        intent.putExtra("allDay", true);
-        intent.putExtra("rrule", "FREQ=YEARLY");
-        intent.putExtra("endTime", cal.getTimeInMillis()+60*60*1000);
-        intent.putExtra("title", "A Test Event from android app");
-        startActivity(intent);
+                // Start the calendar app to add the event
+                startActivity(intent);
+
+
+
+
+
+
+
+
+
+
     }
 }
+
+
